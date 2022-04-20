e8f7d435b8e8c04ae9bebe3fd060c9a53fc4b922
Gerrit Nowald <gerrit.nowald@continental-corporation.com>
2022-04-20 09:57:27 +0200

    list comprehensions, prettier plot

--- dice_combinations.py
+++ dice_combinations.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 plots the probability distribution of 
-the eye sum of arbitrary many, arbitrary sided dice
+the eye-sum of arbitrary many, arbitrary-sided dice
 
 Created on Sun Apr  3 13:11:29 2022
 
@@ -23,17 +23,13 @@ dice = [6,6,20]
 # calculation
 
 # list of lists of all possible eyes for each die
-eyes = []
-for die in dice:
-    eyes.append( [eye+1 for eye in range(die)] )
+eyes = [ [eye+1 for eye in range(die)] for die in dice ]
 
 # all possible result combinations
 combinations = list(itertools.product(*eyes))
 
 # sum of eyes for each combination
-eyesum = []
-for combination in combinations:
-    eyesum.append(sum(combination))
+eyesum = [sum(combination) for combination in combinations]
 
 # probability distribution
 N = np.histogram(eyesum, bins = np.arange( min(eyesum), min(eyesum) + len(set(eyesum)) + 1, 1 ), density=True)
@@ -41,9 +37,19 @@ N = np.histogram(eyesum, bins = np.arange( min(eyesum), min(eyesum) + len(set(ey
 #------------------------------------------------------------------------------
 # plot
 
+plt.close('all')
+
+plt.figure()
+plt.style.use('dark_background')
+
 ax = plt.figure().gca()
-plt.bar(N[1][:-1], N[0], width=0.8)
+plt.bar(N[1][:-1], N[0]*100, width=0.8, color='gold' )
 plt.title('probabilities of eyesums')
 plt.xlabel('eye sum')
-plt.ylabel('probability')
+plt.ylabel('probability / %')
 ax.xaxis.set_major_locator(MaxNLocator(integer=True))   # integer ticks
+
+plt.tight_layout()
+
+plt.show()
+# plt.savefig(f'probability_{dice}.png', transparent=True)
